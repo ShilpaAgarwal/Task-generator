9b631ddd3a3d0d1818117bbb46fd41c339d33886
Shilpa <agarwalshilpa11012001@gmail.com>
2021-05-02 22:46:51 +0530

    Refined the style, added functionality to plus(create task)

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 let filterColor = document.querySelectorAll(".filter");
-let mainContainer = document.querySelector(".main-container")
+let mainContainer = document.querySelector(".main-container");
+let plusBtn = document.querySelector(".plus");
 
-for(let i=0; i<filterColor.length; i++) {
-    filterColor[i].addEventListener("click", function () {
-        
-        let classes = filterColor[i].getAttribute("class");
-        let classArr = classes.split(" ");
-        let color = classArr[1];
-        console.log(color);
+for (let i = 0; i < filterColor.length; i++) {
+  filterColor[i].addEventListener("click", function () {
+    let classes = filterColor[i].getAttribute("class");
+    let classArr = classes.split(" ");
+    let color = classArr[1];
 
-        let mainClasses = mainContainer.getAttribute("class");
-        let mainArr = mainClasses.split(" ");
-        mainArr[1] = color;
-        mainClasses = mainArr.join(" ");
-        mainContainer.setAttribute("class", mainClasses)
-    });
+    let mainClasses = mainContainer.getAttribute("class");
+    let mainArr = mainClasses.split(" ");
+    mainArr[1] = color;
+    mainClasses = mainArr.join(" ");
+    mainContainer.setAttribute("class", mainClasses);
+  });
 }
+
+plusBtn.addEventListener("click", function () {
+  let task = prompt("Enter your task");
+  let color = prompt("Color");
+
+  if (task != "" && color != "") {
+    let div = document.createElement("div");
+    div.setAttribute("class", "ticket-container");
+    div.innerHTML = ` <div class="ticket-color ${color}"></div>
+    <div class="ticket-des-conatiner">
+        <div class="ticket-id">#Example</div>
+        <div class="ticket-des">${task}</div>
+    </div>`
+    
+    mainContainer.appendChild(div);
+  }
+});
