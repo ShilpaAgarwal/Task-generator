93e1ef6f29160215521813246adf48e9596217b7
Shilpa <agarwalshilpa11012001@gmail.com>
2021-05-06 23:42:00 +0530

    Refined the add part, modal formation along with color selector, change of ticket color on clicking its color

--- script.js
+++ script.js
@@ -1,34 +1,67 @@
-let filterColor = document.querySelectorAll(".filter");
+let filterColor = document.querySelectorAll(".filter-color");
 let mainContainer = document.querySelector(".main-container");
+let body = document.querySelector("body");
+let popUp = document.querySelector(".pop-up")
 let plusBtn = document.querySelector(".plus");
+let popInp = document.querySelector(".pop-up-input");
+let popColor = document.querySelectorAll(".color");
+let ticketId = document.querySelectorAll(".ticket-id")
+let colorIn = "black";
+let colorArr = ["pink", "blue", "green", "black"];
 
 for (let i = 0; i < filterColor.length; i++) {
-  filterColor[i].addEventListener("click", function () {
-    let classes = filterColor[i].getAttribute("class");
-    let classArr = classes.split(" ");
-    let color = classArr[1];
+    filterColor[i].addEventListener("click", function () {
+        let classes = filterColor[i].getAttribute("class");
+        let classArr = classes.split(" ");
+        let color = classArr[1];
 
-    let mainClasses = mainContainer.getAttribute("class");
-    let mainArr = mainClasses.split(" ");
-    mainArr[1] = color;
-    mainClasses = mainArr.join(" ");
-    mainContainer.setAttribute("class", mainClasses);
-  });
+        body.setAttribute("class", color);
+    });
 }
 
 plusBtn.addEventListener("click", function () {
-  let task = prompt("Enter your task");
-  let color = prompt("Color");
-
-  if (task != "" && color != "") {
-    let div = document.createElement("div");
-    div.setAttribute("class", "ticket-container");
-    div.innerHTML = ` <div class="ticket-color ${color}"></div>
-    <div class="ticket-des-conatiner">
-        <div class="ticket-id">#Example</div>
-        <div class="ticket-des">${task}</div>
-    </div>`
-    
-    mainContainer.appendChild(div);
-  }
+    popUp.style.display = "flex";
 });
+popInp.addEventListener("keydown", function (e) {
+    
+    if(e.key == "Enter" && popInp.value != "") {
+        let ticket = document.createElement("div");
+        ticket.setAttribute("class", "ticket-container");
+        let task = popInp.value;
+        
+        ticket.innerHTML = ` <div class="ticket-color ${colorIn}"></div>
+        <div class="ticket-des-conatiner">
+            <div class="ticket-id">#Example</div>
+            <div class="ticket-des">${task}</div>
+        </div>`;
+
+        popUp.style.display = "none";
+        popInp.value = ""
+        mainContainer.appendChild(ticket);
+
+        addFunctionality(ticket);
+    }
+})
+for(let i=0; i<popColor.length; i++) {
+    popColor[i].addEventListener("click", function () {
+        let colorTi = popColor[i].classList[1];
+        colorIn = colorTi;
+
+        // remove everyone
+        for(let j=0; j<popColor.length; j++) 
+            popColor[j].classList.remove("border");
+        
+        popColor[i].classList.add("border");
+    })
+}
+function addFunctionality(ticket) {
+    let ticketColor = ticket.querySelector(".ticket-color");
+    ticketColor.addEventListener("click", function () {
+        let cColor = ticketColor.classList[1];
+        let index= colorArr.indexOf(cColor);
+        let newIndex = (index+1)%4;
+
+        ticketColor.classList.remove(cColor);
+        ticketColor.classList.add(colorArr[newIndex]);
+    })
+}
